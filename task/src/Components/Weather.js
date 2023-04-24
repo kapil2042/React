6939aca312395e9bd2861dc0e8691711fff7a147
kapil2042 <kapilpatel2042@gmail.com>
2023-04-24 22:21:44 +0530

    done

--- task/src/Components/Weather.js
+++ task/src/Components/Weather.js
@@ -1,43 +1,90 @@
 import React, { useEffect, useState } from 'react'
 
 function Weather() {
-    const [currentWether, setCurrentWether] = useState({})
-    const [search, setSearch] = useState("Ahmedabad")
-    const fetchCurrentWeather = (city) => {
+    const [currentWether, setCurrentWether] = useState([])
+    const [currentCityWether, setCurrentCityWether] = useState({})
+    const[wCity,setWCity]=useState([])
+    const [search, setSearch] = useState()
+    const fetchWeather = (city) => {
         fetch(`http://api.weatherapi.com/v1/current.json?key=4a018692a1f54a9babe175257231202&q=${city}&aqi=no`)
             .then(response => {
                 return response.json()
             })
             .then(data => {
-                setCurrentWether(data)
+                setCurrentWether([...currentWether,data])
             })
     }
-    // useEffect(() => {
-    //     fetchCurrentWeather(search)
-    // }, { currentWether })
+    const fetchCurrentWeather = async() => {
+     const currentWeather= await fetch(`http://api.weatherapi.com/v1/current.json?key=4a018692a1f54a9babe175257231202&q=Ahmedabad&aqi=no`)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                setCurrentCityWether(data)
+            })
+    }
+
+    useEffect(()=>{
+        fetchCurrentWeather()
+        const localData=localStorage.getItem('cities')
+        const data=JSON.parse(localData)
+        data && setWCity(data)
+    },[])
+
+    useEffect(()=>{
+        return ()=>{
+            wCity && localStorage.setItem('cities',JSON.stringify(wCity))
+        }
+    },[wCity])
+
+    useEffect(()=>{
+        return ()=>{
+            setWCity([...wCity,search])
+        }        
+    },[search])
+
+    useEffect(()=>{
+        wCity.map((item)=>{
+            fetchCurrentWeather(item)
+        })
+    },[search])
     
     const handelSearach = (e)=>{
         e.preventDefault();
         setSearch(e.target.searchbar.value);
-        fetchCurrentWeather(search);
     }
     return (
         <div className='m-5'>
-            {currentWether !== null && (
+            {currentCityWether !== null && (
                 <div className='border p-3'>
-                    <p>Location : {currentWether.location.name}</p>
-                    <p>State : {currentWether.location.region}</p>
-                    <p>Country : {currentWether.location.country}</p>
-                    <p>TEMPERATURE : {currentWether.current.temp_c} 'c</p>
-                    <p>HUMIDITY : {currentWether.current.humidity}</p>
-                    <p>WIND SPEED : {currentWether.current.wind_kph} km/h</p>
-                    <p>PRESSURE : {currentWether.current.pressure_mb} mb</p>
+                    <p>Location : {currentCityWether?.location?.name}</p>
+                    <p>State : {currentCityWether?.location?.region}</p>
+                    <p>Country : {currentCityWether?.location?.country}</p>
+                    <p>TEMPERATURE : {currentCityWether?.current?.temp_c} 'c</p>
+                    <p>HUMIDITY : {currentCityWether?.current?.humidity}</p>
+                    <p>WIND SPEED : {currentCityWether?.current?.wind_kph} km/h</p>
+                    <p>PRESSURE : {currentCityWether?.current?.pressure_mb} mb</p>
                 </div>
             )}
             <form className='mt-3' onSubmit={handelSearach}>
                 <input type='text' id='searchbar' name='searchbar' className='form-control' placeholder='Search city...' />
                 <button type='submit' className='btn btn-primary mt-3'>Search</button>
             </form>
+            {currentWether.length>0 ?
+                currentWether.slice(currentWether.length < 5? 0:currentWether.length-5,currentWether.length).map((item,key)=>{
+                    return(
+                    <div className='border p-3'>
+                        <p>Location : {currentWether?.location?.name}</p>
+                        <p>State : {currentWether?.location?.region}</p>
+                        <p>Country : {currentWether?.location?.country}</p>
+                        <p>TEMPERATURE : {currentWether?.current?.temp_c} 'c</p>
+                        <p>HUMIDITY : {currentWether?.current?.humidity}</p>
+                        <p>WIND SPEED : {currentWether?.current?.wind_kph} km/h</p>
+                        <p>PRESSURE : {currentWether?.current?.pressure_mb} mb</p>
+                    </div>
+                     )
+                }):""
+            } 
         </div>
     )
 }
